3fa8b312d476a740b74e9884572c6266cc1247ad
Jason <JSilvis030592@yahoo.com>
2020-01-14 14:32:12 -0500

    First commit

---
+++ hello_World1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello_World1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello" + " World");
+            string name = "Jason";
+            Console.WriteLine("My name is " + name); // displays name
+            int one = 1;
+            int two = 3;
+            Console.WriteLine(one + two);
+        }
+    }
+}
